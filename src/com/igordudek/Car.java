2038b6acbd26ed628c4b1c0324d519672b2d3cff
igorexx <igordu73@gmail.com>
2017-02-21 11:52:45 +0100

    Car

--- src/com/igordudek/Car.java
+++ src/com/igordudek/Car.java
@@ -4,34 +4,63 @@ package com.igordudek;
  * Created by VADMIN on 2017-02-21.
  */
 class Car {
-    int doors;
-    String carColor;
-    String wheelsColor;
-    String tiresColor;
-    String brand;
-    String model;
 
-    //Konstruktor
-    Car(String carBrand, String carModel, int doorsNumber, String color, String wheels, String tires) {
+    //VARIABLES
+    private int year;
+    private String brand;
+    private String model;
+    private String color;
 
-        brand = carBrand;
-        model = carModel;
-        doors = doorsNumber;
-        carColor = color;
-        wheelsColor = wheels;
-        tiresColor = tires;
+    //CONSTRUCTORS
+    public Car(int year, String brand, String model, String color) {
+
+        this.setYear(year);
+        this.setBrand(brand);
+        this.setModel(model);
+        this.setColor(color);
     }
 
 
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     void printInfo() {
         String info = " ";
         info = info + brand + " ";
         info = info + model + ", ";
-        info = info + "kolor: " + carColor + ", ";
-        info = info + "drzwi: " + doors + ", ";
-        info = info + "felgi: " + wheelsColor + ", ";
-        info = info + "opony: " + tiresColor;
+        info = info + "kolor: " + color + ", ";
+        info = info + "rocznik: " + year + ", ";
+
 
         System.out.println(info);
     }
--- src/com/igordudek/CarShop.java
+++ src/com/igordudek/CarShop.java
@@ -4,8 +4,8 @@ public class CarShop {
 
     public static void main(String[] args) {
 
-        Car audiA4 = new Car("Audi", "A4", 5, "Czarny", "Biale", "Czarne");
-        Car vwPassat = new Car("Volkswagen", "Passat", 5, "niebieski", "Srebrne", "Czarne");
+        Car audiA4 = new Car(2008, "Audi", "A4", "czarny");
+        Car vwPassat = new Car(2015, "Volkswagen", "Passat", "bialy");
 
         audiA4.printInfo();
         vwPassat.printInfo();
