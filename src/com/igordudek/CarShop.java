c0693d26158e7525a26be5205bba1a2a49667090
igor <igordu73@gmail.com>
2017-02-23 12:41:36 +0100

    Dodany Fiat Bravo

--- src/com/igordudek/CarShop.java
+++ src/com/igordudek/CarShop.java
@@ -6,9 +6,11 @@ public class CarShop {
 
         Car audiA4 = new Car(2008, "Audi", "A4", "czarny");
         Car vwPassat = new Car(2015, "Volkswagen", "Passat", "bialy");
+        Car fiatBravo = new Car(2009, "Fiat", "Bravo", "srebrny");
 
         audiA4.printInfo();
         vwPassat.printInfo();
+        fiatBravo.printInfo();
 
 
     }
